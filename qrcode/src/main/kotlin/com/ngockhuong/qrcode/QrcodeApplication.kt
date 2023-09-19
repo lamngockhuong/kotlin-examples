f1a5779afcbc9f9bad2433ff26ea8c72670e60b7
Lam Ngoc Khuong <lam.ngoc.khuong@sun-asterisk.com>
2023-09-19 14:11:12 +0700

    Add qrcode project

---
+++ qrcode/src/main/kotlin/com/ngockhuong/qrcode/QrcodeApplication.kt
@@ -0,0 +1,11 @@
+package com.ngockhuong.qrcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QrcodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<QrcodeApplication>(*args)
+}
---
+++ qrcode/src/test/kotlin/com/ngockhuong/qrcode/QrcodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ngockhuong.qrcode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QrcodeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
